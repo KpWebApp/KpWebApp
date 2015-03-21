8b3271a832cc6af233a0926a8f085c1017a79577
Markiyan Fostyak <markiyan27@yahoo.com>
2015-03-22 00:09:42 +0200

    Temporary removed unitTests

--- KPWebApp.UnitTests/UnitTest1.cs
+++ KPWebApp.UnitTests/UnitTest1.cs
@@ -11,33 +11,33 @@ using Moq;
 
 namespace KPWebApp.UnitTests
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //Arrange
-            Mock<IRepository> mock = new Mock<IRepository>();
-            mock.Setup(m => m.PostsCollection).Returns(new Post[]
-            {
-                new Post {Category = PostCategory.News, Header = "New1", PostId = 1, Text = "SomeText1", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
-                new Post {Category = PostCategory.News, Header = "New2", PostId = 2, Text = "SomeText2", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
-                new Post {Category = PostCategory.News, Header = "New3", PostId = 3, Text = "SomeText3", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
-                new Post {Category = PostCategory.News, Header = "New4", PostId = 4, Text = "SomeText4", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
-                new Post {Category = PostCategory.News, Header = "New5", PostId = 5, Text = "SomeText5", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
-            }.AsQueryable());
-            PostController controller = new PostController(mock.Object);
-            controller.PageCapacity = 3;
+    //[TestClass]
+    //public class UnitTest1
+    //{
+    //    [TestMethod]
+    //    public void TestMethod1()
+    //    {
+    //        //Arrange
+    //        Mock<IRepository> mock = new Mock<IRepository>();
+    //        mock.Setup(m => m.PostsCollection).Returns(new Post[]
+    //        {
+    //            new Post {Category = PostCategory.News, Header = "New1", PostId = 1, Text = "SomeText1", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
+    //            new Post {Category = PostCategory.News, Header = "New2", PostId = 2, Text = "SomeText2", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
+    //            new Post {Category = PostCategory.News, Header = "New3", PostId = 3, Text = "SomeText3", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
+    //            new Post {Category = PostCategory.News, Header = "New4", PostId = 4, Text = "SomeText4", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
+    //            new Post {Category = PostCategory.News, Header = "New5", PostId = 5, Text = "SomeText5", User = new User{Role = Role.Administrator}, Time = DateTime.Now},
+    //        }.AsQueryable());
+    //        PostController controller = new PostController(mock.Object);
+    //        controller.PageCapacity = 3;
 
-            //Act
-            PostsListViewModel result = (PostsListViewModel)controller.List("News", 2).Model;
+    //        //Act
+    //        PostsListViewModel result = (PostsListViewModel)controller.List("News", 2).Model;
 
-            //Assert
-            Post[] postArray = result.Posts.ToArray();
-            Assert.IsTrue(postArray.Length == 2);
-            Assert.AreEqual(postArray[0].Header, "New4");
-            Assert.AreEqual(postArray[1].Header, "New5");
-        }
-    }
+    //        //Assert
+    //        Post[] postArray = result.Posts.ToArray();
+    //        Assert.IsTrue(postArray.Length == 2);
+    //        Assert.AreEqual(postArray[0].Header, "New4");
+    //        Assert.AreEqual(postArray[1].Header, "New5");
+    //    }
+    //}
 }
