c3d2ff54585fecf88d4a3aac0796948791c1cbb9
Markiyan Fostyak <markiyan27@yahoo.com>
2015-03-21 23:23:47 +0200

    Repository and UnitOfWork patterns
    Implemented Repository and UnitOfWork Patterns. Remake PostControllers
    using mantioned patterns.

---
+++ KPWebApp.DAL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPWebApp.DAL
+{
+    public interface IRepository<T> :IDisposable where T : class
+    {
+        T GetById(object id);
+        void Insert(T entity);
+        void Delete(object id);
+        void Update(T entity);
+        void Save();
+    }
+}
---
+++ KPWebApp.DAL/Repository.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using KPWebApp.Domain.Concrete;
+
+namespace KPWebApp.DAL
+{
+    public class Repository<T>:IRepository<T> where T : class
+    {
+        internal KpWebAppDb context;
+        internal DbSet<T> dbSet;
+        private bool disposed = false;
+
+        public Repository(KpWebAppDb ctx)
+        {
+            this.context = ctx;
+            this.dbSet = this.context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> Get(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split(new char[]{','}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public T GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(object id)
+        {
+            T entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(T entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
